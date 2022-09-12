ee568618a5bddc27893ee073ac5a042b2f1ae769
Chrisjkey2332 <Key.christopher@yahoo.com>
2022-09-12 12:40:05 -0500

    Assignment 3 changes

---
+++ module.py
@@ -0,0 +1,10 @@
+def circumferenceFinder(c):
+    #Request input from user
+    print("Enter Radius of Circle: ")
+
+    #Calculate circumferece
+    r = float(input())
+    c = 3.14*r*r
+
+    #Print answer
+    print("\nCircumference = ", c)
